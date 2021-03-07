61de96b38c6cbd0c2d9b143c26de59d0724e8eca
contact.bonychops@gmail.com <contact.bonychops@gmail.com>
2021-03-07 20:35:21 +0900

    Edit: use awesome package..>

--- app.ts
+++ app.ts
@@ -1,22 +1,44 @@
-import { optionChooser } from './src/optionChooser';
-const open = require('open');
+const openLink = require('open');
+const input = require('input');
 
 
 (async () => {
-
     console.log("╔═╦╦══╦═╦╗╔╦══╗╔═╦╦═╦╦═╦══╗");
     console.log("║║║╠║║╣╔╣╚╝║╔╗║║║║║║║║╔╩╗╔╝");
     console.log("║║║╠║║╣╚╣╔╗║╠╣║║║║║║║║╚╗║║");
     console.log("╚╩═╩══╩═╩╝╚╩╝╚╝╚╩═╩╩═╩═╝╚╝");
     console.log("");
-    console.log("Welcome to nicha-nnct-cli!\nPlease select login method.")
-    const a = new optionChooser(["Sign in via your account(default)", "Sign in via personal token"], (index: number, name: string) => {
-        console.log(name, "selected!");
-        if(index === 0){
+
+    console.log("    _   ______________  _____       _   ___   ______________");
+    console.log("   / | / /  _/ ____/ / / /   |     / | / / | / / ____/_  __/");
+    console.log("  /  |/ // // /   / /_/ / /| |    /  |/ /  |/ / /     / /   ");
+    console.log(" / /|  // // /___/ __  / ___ |   / /|  / /|  / /___  / /    ");
+    console.log("/_/ |_/___/\____/_/ /_/_/  |_|  /_/ |_/_/ |_/\____/ /_/     ");
+
+
+    console.log("");
+    console.log("Welcome to nicha-nnct-cli!");
+    const option = await input.select("Please select login method.", [
+        { name: "Sign in via your account (I'm sorry but not supported yet)", value: "account", disabled: true },
+        { name: "Sign in via personal token", value: "token", default: true },
+        { name: "Exit", value: "exit" }
+    ]);
+
+    console.log(option, "selected!");
+    await ({
+        account: () => {
             console.log("Please open link below to login.\n");
-            const link: string = "https://auth.nicha-nnct.com/";
+            const link: string = "https://auth.nicha-nnct.web.app/";
             console.log(link);
-            open(link);
+            openLink(link);
+        },
+        token: async () => {
+            const link: string = "https://docs.nicha-nnct.web.app/auth/personal-token";
+            console.log(`You can login via your token. Learm more: ${link}`);
+            const token = input.password("Input token here: ")
+        },
+        exit: () => {
+            process.exit(0);
         }
-    })
+    })[option]();
 })();
